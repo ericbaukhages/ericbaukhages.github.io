9c877ec52483831aafdc7c81a5aff1a6c8bf08df
Eric Baukhages <ebaukhages@thecargoagency.com>
2019-10-11 00:38:25 -0400

    Start of something new

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # febvigrail.github.io
-Blog
+
+Generate CSS:
+
+```
+npx tailwind build styles.css -o output.css -c tailwind.config.js
+```
+
+Watch command:
+
+```
+ls tailwind.config.js styles.css | entr npx tailwind build styles.css -o output.css -c tailwind.config.js
+```
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        "eb-grey": "#483c46",
+        "eb-teal": "#3c6e71",
+        "eb-green": "#70ae6e",
+        "eb-light-green": "#beee62",
+        "eb-orange": "#f4743b",
+      },
+      fontFamily: {
+        "press": "'Press Start 2P', monospace",
+        "vt323": "'VT323', monospace",
+      },
+    }
+  },
+  variants: {},
+  plugins: []
+}
